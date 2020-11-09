dab475c6aa607ba651551e2c1af434d867d41c72
PraveshGurung <s0174500@ad.ua.ac.be>
2020-11-09 12:13:29 +0100

    more clean up

--- src/lucene/IndexFiles.java
+++ src/lucene/IndexFiles.java
@@ -24,8 +24,6 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.similarities.BM25Similarity;
-import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.w3c.dom.Node;
@@ -51,19 +49,15 @@ public class IndexFiles {
     /** Index all text files under a directory. */
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String usage = "java org.apache.lucene.demo.IndexFiles"
-                + " [-index INDEX_PATH] [-docs DOCS_PATH or -xml XML_FILE_PATH] [-update]\n\n"
-                + "This indexes the documents in DOCS_PATH, creating a Lucene index"
+                + " [-index INDEX_PATH] [-xml XML_FILE_PATH] [-update]\n\n"
+                + "This indexes the xml file in XML_FILE_PATH, creating a Lucene index"
                 + "in INDEX_PATH that can be searched with SearchFiles";
         String indexPath = "index";
-        String docsPath = null;
         String xmlPath = null;
         boolean create = true;
         for(int i=0;i<args.length;i++) {
             if ("-index".equals(args[i])) {
                 indexPath = args[i+1];
-                i++;
-            } else if ("-docs".equals(args[i])) {
-                docsPath = args[i+1];
                 i++;
             } else if ("-update".equals(args[i])) {
                 create = false;
@@ -73,24 +67,16 @@ public class IndexFiles {
             }
         }
 
-        if (docsPath == null && xmlPath == null) {
+        if (xmlPath == null) {
             System.err.println("Usage: " + usage);
             System.exit(1);
         }
 
         Path xmlFile=null;
-        Path docDir=null;
         if (xmlPath!=null){
             xmlFile = Paths.get(xmlPath);
             if (!Files.isReadable(xmlFile)) {
                 System.out.println("xml file '" +xmlFile.toAbsolutePath()+ "' does not exist or is not readable, please check the file");
-                System.exit(1);
-            }
-        }
-        else {
-            docDir = Paths.get(docsPath);
-            if (!Files.isReadable(docDir)) {
-                System.out.println("Document directory '" + docDir.toAbsolutePath() + "' does not exist or is not readable, please check the path");
                 System.exit(1);
             }
         }
@@ -113,12 +99,7 @@ public class IndexFiles {
             }
 
             IndexWriter writer = new IndexWriter(dir, iwc);
-            if(xmlPath==null){
-                indexDocs(writer, docDir);
-            }
-            else{
-                indexXml(writer,xmlPath);
-            }
+            indexXml(writer,xmlPath);
 
             writer.close();
 
@@ -179,68 +160,6 @@ public class IndexFiles {
                     System.out.println("updating " + title);
                     writer.updateDocument(new Term("title", file.toString()), doc);
                 }
-            }
-        }
-    }
-
-    /**
-     * Indexes the given file using the given writer, or if a directory is given,
-     * recurses over files and directories found under the given directory.
-     *
-     * @param writer Writer to the index where the given file/dir info will be stored
-     * @param path The file to index, or the directory to recurse into to find files to index
-     * @throws IOException If there is a low-level I/O error
-     */
-    static void indexDocs(final IndexWriter writer, Path path) throws IOException {
-        if (Files.isDirectory(path)) {
-            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    try {
-                        indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
-                    } catch (IOException ignore) {
-                        // don't index files that can't be read.
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
-        } else {
-            indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
-        }
-    }
-
-    /** Indexes a single document */
-    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
-        try (InputStream stream = Files.newInputStream(file)) {
-            // make a new, empty document
-            Document doc = new Document();
-
-            // Add the path of the file as a field named "title".  Use a
-            // field that is indexed (i.e. searchable), but don't tokenize
-            // the field into separate words and do index term frequency and positional information
-            Field pathField = new TextField("title", file.getFileName().toString(), Field.Store.YES);
-            doc.add(pathField);
-
-            // Add the contents of the file to a field named "contents".  Specify a Reader,
-            // so that the text of the file is tokenized and indexed, but not stored.
-            StringBuilder content= new StringBuilder();
-            var reader=new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
-            String line;
-            while ((line=reader.readLine())!=null){
-                content.append(line).append("\n");
-            }
-            doc.add(new TextField("contents", content.toString(), Field.Store.YES));
-
-            if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-                // New index, so we just add the document (no old document can be there):
-                System.out.println("adding " + file);
-                writer.addDocument(doc);
-            } else {
-                // Existing index (an old copy of this document may have been indexed) so
-                // we use updateDocument instead to replace the old one matching the exact
-                // path, if present:
-                System.out.println("updating " + file);
-                writer.updateDocument(new Term("title", file.toString()), doc);
             }
         }
     }
